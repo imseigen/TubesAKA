88ff2d474d5e30f4a6e2dfa899c0c64fec24deb2
imseigen <seigenjane@gmail.com>
2024-12-27 21:40:21 +0700

    Add files via upload

--- TubesAKA/main.cpp
+++ TubesAKA/main.cpp
@@ -1,14 +1,25 @@
 #include "towerofhanoi.h"
+#include <chrono>
 
 int main() {
-    int n = 5;  // Menetapkan jumlah cakram menjadi 5
+     int n = 5;  // Menetapkan jumlah cakram menjadi 5
     char dari = 'A', ke = 'B', bantu = 'C';
 
+    // Rekursif
     cout << "\nSolusi menggunakan rekursif:\n";
+    auto startRekursif = chrono::high_resolution_clock::now();
     hanoiRekursif(n, dari, ke, bantu);
+    auto endRekursif = chrono::high_resolution_clock::now();
+    chrono::duration<double> rekursifDurasi = endRekursif - startRekursif;
+    cout << "Waktu eksekusi rekursif: " << rekursifDurasi.count() << " detik\n";
 
+    // Iteratif
     cout << "\nSolusi menggunakan iteratif:\n";
+    auto startIteratif = chrono::high_resolution_clock::now();
     hanoiIteratif(n, dari, ke, bantu);
+    auto endIteratif = chrono::high_resolution_clock::now();
+    chrono::duration<double> iteratifDurasi = endIteratif - startIteratif;
+    cout << "Waktu eksekusi iteratif: " << iteratifDurasi.count() << " detik\n";
 
     return 0;
 }
